5e512d9cf1786fb0d199db290c0222d743c7e79e
alexander <ezhkovskii@gmail.com>
2020-03-16 17:56:03 +0500

    Вывод котировок валют и металлов на три дня

--- api.py
+++ api.py
@@ -2,6 +2,21 @@ import requests
 import datetime
 import xml.etree.ElementTree as ET
 
+def GetDateToday():
+    #yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
+    today = datetime.datetime.today()
+    if today.day < 10:
+        day = '0' + str(today.day)
+    else:
+        day = str(today.day)
+    if today.month < 10:
+        month = '0' + str(today.month)
+    else:
+        month = str(today.month)
+    year = str(today.year)
+    DateToday = day + "/" + month + "/" + year
+    return DateToday
+
 def GetDateYesterday():
     yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
     if yesterday.day < 10:
@@ -15,6 +30,20 @@ def GetDateYesterday():
     year = str(yesterday.year)
     DateYesterday = day + "/" + month + "/" + year
     return DateYesterday
+
+def GetDateTomorrow():
+    tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
+    if tomorrow.day < 10:
+        day = '0' + str(tomorrow.day)
+    else:
+        day = str(tomorrow.day)
+    if tomorrow.month < 10:
+        month = '0' + str(tomorrow.month)
+    else:
+        month = str(tomorrow.month)
+    year = str(tomorrow.year)
+    DateTomorrow = day + "/" + month + "/" + year
+    return DateTomorrow
 
 
 def GetDollarAndEuro(date):
@@ -53,15 +82,19 @@ def GetGold(date):
         result['silver'] = 'Неизвестно'
         return result
 
+DateTomorrow = GetDateTomorrow()
+DateToday = GetDateToday()
 DateYesterday = GetDateYesterday()
+Dates = [GetDateTomorrow(), GetDateToday(), GetDateYesterday()]
 
-Valute = GetDollarAndEuro(DateYesterday)
-Metall = GetGold(DateYesterday)
-
-print('Курсы валют на %s:' % (DateYesterday))
-print('%s: %s' % ('Доллар', Valute['dollar']))
-print('%s: %s' % ('Евро', Valute['euro']))
-
-print('Котировки драгоценных металлов на %s:' % (DateYesterday))
-print('%s: %s' % ('Золото', Metall['gold']))
-print('%s: %s' % ('Евро', Metall['silver']))
+for date in Dates:
+    Valute = GetDollarAndEuro(date)
+    Metall = GetGold(date)
+    print('Курсы валют на %s:' % (date))
+    print('%s: %s' % ('Доллар', Valute['dollar']))
+    print('%s: %s' % ('Евро', Valute['euro']))
+    print()
+    print('Котировки драгоценных металлов на %s:' % (date))
+    print('%s: %s' % ('Золото', Metall['gold']))
+    print('%s: %s' % ('Евро', Metall['silver']))
+    print()
