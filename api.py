6b1d27c171292ad778e886e4e32c8918d5c87bfe
Alexander <ezhkovskii@gmail.com>
2020-08-14 18:31:29 +0500

    Немного исправил код согласено pep-8. Поменял формат вывода курса валют и котировок. Теперь вывод происходит ввиде таблицы.

--- api.py
+++ api.py
@@ -2,8 +2,11 @@ import requests
 import datetime
 import xml.etree.ElementTree as ET
 
-def GetDateToday():
-    #yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
+
+'''
+Функция возвращает текущую дату в формате 01/01/1900
+'''
+def get_date_today():
     today = datetime.datetime.today()
     if today.day < 10:
         day = '0' + str(today.day)
@@ -14,10 +17,14 @@ def GetDateToday():
     else:
         month = str(today.month)
     year = str(today.year)
-    DateToday = day + "/" + month + "/" + year
-    return DateToday
+    date_today = day + "/" + month + "/" + year
+    return date_today
 
-def GetDateYesterday():
+
+'''
+Функция возвращает вчерашнюю дату в формате 01/01/1900
+'''
+def get_date_yesterday():
     yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
     if yesterday.day < 10:
         day = '0' + str(yesterday.day)
@@ -28,10 +35,14 @@ def GetDateYesterday():
     else:
         month = str(yesterday.month)
     year = str(yesterday.year)
-    DateYesterday = day + "/" + month + "/" + year
-    return DateYesterday
+    date_yesterday = day + "/" + month + "/" + year
+    return date_yesterday
 
-def GetDateTomorrow():
+
+'''
+Функция возвращает завтрашнюю дату в формате 01/01/1900
+'''
+def get_date_tomorrow():
     tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
     if tomorrow.day < 10:
         day = '0' + str(tomorrow.day)
@@ -42,16 +53,19 @@ def GetDateTomorrow():
     else:
         month = str(tomorrow.month)
     year = str(tomorrow.year)
-    DateTomorrow = day + "/" + month + "/" + year
-    return DateTomorrow
+    date_tomorrow = day + "/" + month + "/" + year
+    return date_tomorrow
 
 
-def GetDollarAndEuro(date):
+'''
+Функция возвращает словарь с элементами доллар и евро
+'''
+def get_dollar_and_euro(date):
     result = {
         'dollar': '',
         'euro': '',
     }
-    r = requests.get('http://www.cbr.ru/scripts/XML_daily.asp?date_req=%s'  % (date))
+    r = requests.get('http://www.cbr.ru/scripts/XML_daily.asp?date_req=%s' % (date))
     structure = ET.fromstring(r.content)
     try:
         dollar = structure.find("./*[@ID='R01235']/Value")
@@ -64,12 +78,16 @@ def GetDollarAndEuro(date):
         result['euro'] = 'Неизвестно'
         return result
 
-def GetGoldSilver(date):
+
+'''
+Функция возвращает словарь с элементами золото и серебро
+'''
+def get_gold_and_silver(date):
     result = {
         'gold': '',
         'silver': '',
     }
-    r = requests.get('http://www.cbr.ru/scripts/xml_metall.asp?date_req1=%s&date_req2=%s' % (date,date))
+    r = requests.get('http://www.cbr.ru/scripts/xml_metall.asp?date_req1=%s&date_req2=%s' % (date, date))
     structure = ET.fromstring(r.content)
     try:
         gold = structure.find("./*[@Code='1']/Sell")
@@ -82,15 +100,33 @@ def GetGoldSilver(date):
         result['silver'] = 'Неизвестно'
         return result
 
-Dates = [GetDateTomorrow(), GetDateToday(), GetDateYesterday()]
 
-for date in Dates:
-    Valute = GetDollarAndEuro(date)
-    Metall = GetGoldSilver(date)
+'''
+Процедура выводит курсы валют и котировки драгоценных металлов на определенную дату
+'''
+def print_currency_and_metall(currency, metall, date):
+    # Вывод ввиде таблицы
+    result = "%10s\t%10s\t%10s\t%10s\t%10s" % (
+    date, currency['dollar'], currency['euro'], metall['gold'], metall['silver'])
+    print(result)
+
+    '''
     print('Курсы валют и котировки драгоценных металлов на %s:' % (date))
-    print('%s: %s' % ('Доллар', Valute['dollar']))
-    print('%s: %s' % ('Евро', Valute['euro']))
-    print('%s: %s' % ('Золото', Metall['gold']))
-    print('%s: %s' % ('Серебро', Metall['silver']))
+    print('%s: %s' % ('Доллар', currency['dollar']))
+    print('%s: %s' % ('Евро', currency['euro']))
+    print('%s: %s' % ('Золото', metall['gold']))
+    print('%s: %s' % ('Серебро', metall['silver']))
     print()
+    '''
 
+
+if __name__ == "__main__":
+
+    dates = [get_date_tomorrow(), get_date_today(), get_date_yesterday()]
+
+    print("%10s\t%10s\t%10s\t%10s\t%10s" % ("Дата", "Доллар", "Евро", "Золото", "Серебро"))
+    for date in dates:
+        currency = get_dollar_and_euro(date)
+        metall = get_gold_and_silver(date)
+
+        print_currency_and_metall(currency, metall, date)
